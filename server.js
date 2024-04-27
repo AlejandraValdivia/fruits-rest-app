5b45a1df32c7381672b5b23f75ea9fb3d3753ee4
Rome Bell <romebell5@gmail.com>
2024-04-27 15:23:13 -0700

    feat: add POST route with new GET route

--- server.js
+++ server.js
@@ -9,12 +9,19 @@ const { fruits } = require('./models/fruits');
 // ------------ MIDDLEWARE ------------
 app.set('view engine', 'ejs');
 app.use('/', express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ------------ ROUTES ---------------
 // ******* INDEX ROUTE **********
 app.get('/fruits', (req, res) => {
     // send index.ejs with array of fruits
     res.render('fruits/index', { allFruits: fruits });
+});
+
+// ********** NEW ROUTE **********
+app.get('/fruits/new', (req, res) => {
+    res.render('fruits/new.ejs', {});
 });
 
 // ******* SHOW ROUTE **********
@@ -28,6 +35,19 @@ app.get('/fruits/:indexOfFruitsArray', (req, res) => {
         // res.send(fruits[idx]);
         res.render('fruits/show', { fruit: fruits[idx] });
     }
+});
+
+// ********** POST NEW FRUIT ************
+app.post('/fruits', (req, res) => {
+    console.log('---------- FORM BODY ---------\n', req.body);
+    // add more code here
+    if (req.body.readyToEat === 'on') {
+        req.body.readyToEat = true;
+    } else { // req.body.readyToEat will be undefined (unchecked)
+        req.body.readyToEat = false;
+    }
+    fruits.push(req.body);
+    res.redirect('/fruits');
 });
 
 // ----------- LISTEN FOR SERVER ----------
